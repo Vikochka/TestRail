96f7eefc1ede07537f019a418321cd13abdfa991
Viktoria <62216160+Vikochka@users.noreply.github.com>
2021-05-16 20:51:29 +0300

    Fixed testRun test

--- src/test/java/test/TestRunTest.java
+++ src/test/java/test/TestRunTest.java
@@ -5,13 +5,12 @@ import org.testng.annotations.Test;
 public class TestRunTest extends BaseTest {
 
     @Test(description = "Create Test Run")
-    public void addTestRunTest() {
+    public void addTestRun() {
         loginStep.login(login, password);
         administrationSteps.admin("Project");
-       // projectSteps.addProject("Use multiple test", "TestRail", "Diploma project");
+        // projectSteps.addProject("Use multiple test", "TestRail", "Diploma project");
+
         projectPage.clickSmallIcon("TestRail");
-        testRunSteps.addTestRun("TestRail","Login"," ","Me","blabla","Include all test cases");
+        testRunSteps.addTestRun("Login", " ", " ", "Me", "blabla", "Include all test cases");
     }
-
-
 }
